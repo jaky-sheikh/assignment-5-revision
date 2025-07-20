ee7960e974f9fa4b91d4c6c1782bdc35400e7e92
jaky-sheikh <jaky@eu4m.eu>
2025-07-20 16:17:00 +0600

    add money btn functionality done with amount

--- Js/addMoney.js
+++ Js/addMoney.js
@@ -1,5 +1,12 @@
-console.log("Add money btn")
+// console.log("Add money btn")
 document.getElementById('add-money-btn').addEventListener('click', function (event) {
     event.preventDefault();
-    console.log('btn clicked');
+    // console.log('btn clicked');
+    const addMoney = document.getElementById('add-amount').innerText;
+    // console.log(addMoney);
+    const addMoneyNumber = parseFloat(addMoney);
+    // console.log(addMoneyNumber);
+    const newBalance = addMoneyNumber + 25000;
+    // console.log(newBalance);
+    document.getElementById('add-amount').innerText = newBalance;
 })
