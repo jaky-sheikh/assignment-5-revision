8f776c381b548be3d120a53b7811749199893f17
jaky-sheikh <jaky@eu4m.eu>
2025-07-20 21:20:35 +0600

    total donation amount added functionality

--- Js/addMoney.js
+++ Js/addMoney.js
@@ -9,6 +9,7 @@ document.getElementById('add-money-btn').addEventListener('click', function (eve
     // console.log(addMoneyNumber);
     const newBalance = addMoneyNumber + 25000;
     // console.log(newBalance);
+    // set the newBalance in UI 
     document.getElementById('add-amount').innerText = newBalance;
 })
 // Functionality for donate money btn
@@ -16,20 +17,27 @@ document.getElementById('donate-btn')
     .addEventListener('click', function () {
         // console.log('donate btn clicked');
         const lessMoney = document.getElementById('add-amount').innerText;
-        // console.log(lessMoney);
+        const totalDonateAmount = document.getElementById('total-donate-amount').innerText;
+        // console.log(lessMoney, totalDonateAmount);
         const donateMoney = document.getElementById('donate-amount-input').value;
         // console.log(donateMoney);
         const lessMoneyNumber = parseFloat(lessMoney);
         const donateMoneyNumber = parseFloat(donateMoney);
-        // console.log(lessMoneyNumber, donateMoneyNumber);
+        const totalDonateAmountNumber = parseFloat(totalDonateAmount);
+        // console.log(lessMoneyNumber, donateMoneyNumber, totalDonateAmountNumber);
+        const newDonateBalance = donateMoneyNumber + totalDonateAmountNumber;
+        // console.log(newDonateBalance);
         const newBalance = lessMoneyNumber - donateMoneyNumber;
         if (donateMoneyNumber > lessMoneyNumber) {
             alert('You have not sufficient balance.');
             return;
         }
         else {
-            alert('Done')
+            alert('Congratulations! Your donation is completed.')
         }
         // console.log(newBalance);
         document.getElementById('add-amount').innerText = newBalance;
+
+        document.getElementById('total-donate-amount').innerText = newDonateBalance;
+
     })
